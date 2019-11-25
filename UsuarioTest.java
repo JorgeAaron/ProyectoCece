3bf36f19a72206828871efb5ab2936478232ad09
JorgeAaron <jorgeleones09@gmail.com>
2019-11-25 00:26:43 -0700

    Add files via upload

---
+++ UsuarioTest.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.util.List;
+import mx.itson.proyecto.entidades.Usuario;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Jorge
+ */
+
+
+
+
+public class UsuarioTest {
+   
+           
+  Usuario u = new Usuario();
+  
+  @Test
+  public void ObtenerTest() {
+    List<Usuario> esperado = u.ObtenerTodos();
+    List<Usuario> resulatdo = 
+    
+  }
+  
+  @Test
+  public void LoginTest() {
+      
+  }
+  
+  @Test
+  public void 
+}
